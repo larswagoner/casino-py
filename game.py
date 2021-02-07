3908316e534c6db9589059296b44ea43d1e528b9
maxgallup <m.gallup@student.vu.nl>
2021-02-07 23:21:12 +0100

    reverted to latest working version

--- game.py
+++ game.py
@@ -3,36 +3,40 @@ from casino import *
 
 
 
-
 players = []
 players.append(Player("Lars"))
 players.append(Player("Max"))
 
-
-
 deck = create_deck()
 
-for card in deck[10:14:]:
+for card in deck[0:4:]:
     players[0].hand += [card]
 
 for card in deck[4:8:]:
     players[1].hand += [card]
 
-deal_to_center(deck, 4, centerPile)
-
-
-prettyPrint(players, center.list)
 
 
 
 
-# move_to_center(players[0], 0, centerPile)
-# move_to_center(players[0], 1, centerPile)
-# # move_to_center(players[0], 2, centerPile)
-# prettyPrint(players, center.list)
-# moveFromCenter(players[0], 0)
-# moveFromCenter(players[0], 1)
-# # moveFromCenter(players[0], 2)
-# prettyPrint(players, center.list)
+for card in players[0].hand:
+   print(card.return_card())
+ 
 
 
+move_to_center(players[0], 3, centerPile)
+
+print('----------------------')
+
+print(center[0].pile[0].return_card())
+
+print('----------------------')
+
+for card in players[0].hand:
+   print(card.return_card())
+
+moveFromCenter(players[0], 0)
+
+print('----------------------')
+
+print(players[0].discard[0].return_card())
