b18a30fe4acdbda54bba8e40b548a5965478bf34
maxgallup <m.gallup@student.vu.nl>
2021-02-12 16:01:36 +0100

    ignore

--- server/server.js
+++ server/server.js
@@ -10,15 +10,27 @@ httpServer.listen(3100, () => console.log("listening on 3100"));
 
 const ws = new wsServer({
     "httpServer": httpServer
-})
+}) 
 
 ws.on("request", request => {
     //connection by client
     const connection = request.accept(null, request.origin);
     connection.on("open", () => console.log("opened"));
     connection.on("close", () => console.log("closed"));
-    connection.on("message", () => console.log("opened!"));
+    connection.on("message", message => {
+        //Received a message from client
+
+
+    });
+
+
+
 
 
 })
+ 
+function S4() {
+    return (((1+Math.random())*0x10000))
+}
+
 
